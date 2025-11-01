792adcb5cffc34ff96a5902f2077e38dce8eb90c
Samir Warsi <samirwarsi@Samirs-MacBook-Air.local>
2025-11-01 16:04:35 +0530

    final

--- frontend/src/utils/Backendurl.js
+++ frontend/src/utils/Backendurl.js
@@ -1,1 +1,2 @@
-export const BackendUrl = "http://localhost:3000/api/v1"
+// export const BackendUrl = "http://localhost:3000/api/v1"
+export const BackendUrl = "https://threew-3rcs.onrender.com/api/v1"
